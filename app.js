494e45bad20adbb963ef7d8146c23517eb071f04
Ryan Seys <ryanseys@google.com>
2018-11-23 17:09:25 -0800

    cleanup

--- app.js
+++ app.js
@@ -9,15 +9,13 @@ const user = require('./routes/user');
 const http = require('http');
 const path = require('path');
 
-var favicon = require('serve-favicon')
-var logger = require('morgan')
-var methodOverride = require('method-override')
-var session = require('express-session')
-var bodyParser = require('body-parser')
-var multer = require('multer')
-var errorHandler = require('errorhandler')
-
-var app = express();
+const favicon = require('serve-favicon')
+const logger = require('morgan')
+const methodOverride = require('method-override')
+const session = require('express-session')
+const bodyParser = require('body-parser')
+const errorHandler = require('errorhandler')
+const app = express();
 
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'))
@@ -27,16 +25,9 @@ app.use(methodOverride())
 app.use(bodyParser.json())
 
 app.use(bodyParser.urlencoded({ extended: true }))
-// app.use(multer())
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.configure('development', function() {
-//   app.use(express.errorHandler());
-// });
-
 app.get('/', routes.index);
-
-// router.get('/users', user.list);
 
 var server = http.createServer(app)
 server.listen(app.get('port'), function () {
